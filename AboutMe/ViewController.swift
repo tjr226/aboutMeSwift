fa222cc48f34ae884073d493b096a15818236a19
tjr226 <tjr226@gmail.com>
2020-01-05 18:41:57 -0500

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Timothy Rooney on 1/5/20.
+//  Copyright © 2020 Timothy Rooney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
